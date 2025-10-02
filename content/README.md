45cd32214d08a5898909460500121d9c5cb1fd27
User <user@macbookair.speedport.ip>
2025-10-02 18:04:45 +0200

    Quartz sync: Oct 2, 2025, 6:04 PM

---
+++ content/README.md
@@ -0,0 +1,14 @@
+## Seitenaufbau
+Bitte folgende Bausteine nutzen:
+
+> [!danger]+ Rapid Reference
+
+> [!info] Definition
+
+> [!dd] DD
+
+> [!red-flags] Red Flags
+
+> [!anamnese]
+
+> [!]
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Notfallmedizin",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "de-DE",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
