e7a1fc187d1744d6cbfc64d3257462f769d73d83
User <user@MacBook-Air-Vincent.local>
2025-10-07 19:43:52 +0200

    Quartz sync: Oct 7, 2025, 7:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "de-DE",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Privat"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
