b8e391c49644a2f185506cafcb759306cf0c3b7f
User <user@macbookair.speedport.ip>
2025-10-18 13:26:19 +0200

    Quartz sync: Oct 18, 2025, 1:26 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -53,20 +53,12 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
   }*/
 
   function ContentMetadata({ cfg, fileData, displayClass }: QuartzComponentProps) {
-      const segments: (string | JSX.Element)[] = []
-
-      if (fileData.dates) {
-        segments.push("Zuletzt geändert: ")
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
-        segments.push(", ")
-      }
-      segments.push(<span class="localBlock" data-option="default">Hier könnte man zum Beispiel auch was Lokales einfügen</span>)
-      segments.push(<span class="localBlock" data-option="cbf" style="display:none;">OA Notfallmedizin CBF: 552 880</span>)
-      segments.push(<span class="localBlock" data-option="zbb" style="display:none;">Alternative text version ZBB.</span>)
-
       return (
         <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
-          {segments}
+          Zuletzt geändert: <Date date={getDate(cfg, fileData)!} locale={cfg.locale} />, 
+          <span class="localBlock" data-option="default">Hier könnte man zum Beispiel auch was Lokales einfügen</span>
+          <span class="localBlock" data-option="cbf" style="display:none;">OA Notfallmedizin CBF: 552 880</span>)
+          <span class="localBlock" data-option="zbb" style="display:none;">Alternative text version ZBB.</span>)
         </p>
       )
     }
